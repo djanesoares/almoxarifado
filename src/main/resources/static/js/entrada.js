5ab0813dd30cbd8bd3fe24f7c59318ff945430bf
Djane Soares <djanesoares@gmail.com>
2025-04-19 12:28:11 +0100

    corrigido link do home, corrigido background, corrigido paginação entrada e saidas de materiais

--- src/main/resources/static/js/entrada.js
+++ src/main/resources/static/js/entrada.js
@@ -39,3 +39,23 @@ $(document).ready(function () {
     modal.hide();
   });
 });
+
+
+$(document).ready(function () {
+  $('#tabelaEntradas').DataTable({
+    language: {
+      "lengthMenu": "Mostrar _MENU_ registros por página",
+      "zeroRecords": "Nenhum resultado encontrado",
+      "info": "Mostrando página _PAGE_ de _PAGES_",
+      "infoEmpty": "Nenhum registro disponível",
+      "infoFiltered": "(filtrado de _MAX_ registros no total)",
+      "search": "Pesquisar:",
+      "paginate": {
+        "first": "Primeira",
+        "last": "Última",
+        "next": "Próxima",
+        "previous": "Anterior"
+      }
+    }
+  });
+});
--- src/main/resources/static/js/saida.js
+++ src/main/resources/static/js/saida.js
@@ -39,3 +39,22 @@ $(document).ready(function () {
     modal.hide();
   });
 });
+
+$(document).ready(function () {
+  $('#tabelaSaidas').DataTable({
+    language: {
+      "lengthMenu": "Mostrar _MENU_ registros por página",
+      "zeroRecords": "Nenhum resultado encontrado",
+      "info": "Mostrando página _PAGE_ de _PAGES_",
+      "infoEmpty": "Nenhum registro disponível",
+      "infoFiltered": "(filtrado de _MAX_ registros no total)",
+      "search": "Pesquisar:",
+      "paginate": {
+        "first": "Primeira",
+        "last": "Última",
+        "next": "Próxima",
+        "previous": "Anterior"
+      }
+    }
+  });
+});
